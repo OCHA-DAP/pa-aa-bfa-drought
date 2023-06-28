77a92214f70de45f06d480d2b8ab3e1aa3b87ab7
Tristan Downing <downing.tristan@gmail.com>
2023-06-28 07:39:07 -0700

    process cmorph

--- src/download_cmorph.py
+++ src/download_cmorph.py
@@ -6,7 +6,7 @@ import datetime
 import urllib.request
 
 load_dotenv()
-SAVE_DIR = Path(os.environ["SAVE_DIR"])
+CMORPH_DIR = Path(os.environ["CMORPH_DIR"])
 BASE_URL = "https://www.ncei.noaa.gov/pub/data/nidis/test/cmorph/"
 
 start_date = datetime.date(2020, 5, 1)
@@ -17,5 +17,5 @@ dates = pd.date_range(start_date, end_date, freq="M")
 for date in dates:
     filename = f"cmorph_spi_gamma_30_day_{date:%Y-%m-%d}.nc"
     url = BASE_URL + filename
-    save_path = SAVE_DIR / filename
+    save_path = CMORPH_DIR / filename
     urllib.request.urlretrieve(url, save_path)
