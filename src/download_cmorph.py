c6a205ad635dfe7180b99505ad7e386309946ae1
Tristan Downing <downing.tristan@gmail.com>
2023-06-28 18:08:08 -0700

    plot SPI recurrence

--- src/download_cmorph.py
+++ src/download_cmorph.py
@@ -9,13 +9,17 @@ load_dotenv()
 CMORPH_DIR = Path(os.environ["CMORPH_DIR"])
 BASE_URL = "https://www.ncei.noaa.gov/pub/data/nidis/test/cmorph/"
 
+# Daily
 start_date = datetime.date(2020, 5, 1)
-end_date = datetime.date(2020, 8, 1)
-
-dates = pd.date_range(start_date, end_date, freq="M")
+end_date = datetime.date(2023, 1, 1)
+dates = pd.date_range(start_date, end_date, freq="W")
 
 for date in dates:
+    print(date)
     filename = f"cmorph_spi_gamma_30_day_{date:%Y-%m-%d}.nc"
     url = BASE_URL + filename
-    save_path = CMORPH_DIR / filename
-    urllib.request.urlretrieve(url, save_path)
+    save_path = CMORPH_DIR / "daily" / filename
+    try:
+        urllib.request.urlretrieve(url, save_path)
+    except Exception as e:
+        print(e)
