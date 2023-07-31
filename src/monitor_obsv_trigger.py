df7a618e8855307e0d53b6bc42ddc615361cba35
Tristan Downing <downing.tristan@gmail.com>
2023-07-31 12:18:24 -0700

    setup spi class

--- src/monitor_obsv_trigger.py
+++ src/monitor_obsv_trigger.py
@@ -1,22 +1,14 @@
 import datetime
-import pandas as pd
+import os
+from pathlib import Path
+
 import xarray as xr
-import numpy as np
-import itertools
-from rasterio.enums import Resampling
+from dotenv import load_dotenv
+
+# from ochanticipy import CodAB, GeoBoundingBox, create_country_config
 
 iso3 = "bfa"
 adm_sel = ["Boucle du Mouhoun", "Nord", "Centre-Nord", "Sahel"]
-
-from ochanticipy import (
-    create_country_config,
-    CodAB,
-    GeoBoundingBox,
-)
-
-from dotenv import load_dotenv
-from pathlib import Path
-import os
 
 load_dotenv()
 
@@ -29,14 +21,19 @@ def compute_spi_trigger():
     date = datetime.date(2023, 7, 26)
     filename = f"bfa-NOAA_CPC_CMORPH-spi-1mo_{date}.nc"
     ds = xr.open_dataset(SPI_PROC_DIR / filename)
-    country_config = create_country_config(iso3=iso3)
-    codab = CodAB(country_config=country_config)
-    gdf_adm1 = codab.load(admin_level=1)
-    gdf_aoi = gdf_adm1[gdf_adm1["ADM1_FR"].isin(adm_sel)]
-    geobb = GeoBoundingBox.from_shape(gdf_adm1)
+    # country_config = create_country_config(iso3=iso3)
+    # codab = CodAB(country_config=country_config)
+    # gdf_adm1 = codab.load(admin_level=1)
+    # gdf_aoi = gdf_adm1[gdf_adm1["ADM1_FR"].isin(adm_sel)]
+    # geobb = GeoBoundingBox.from_shape(gdf_adm1)
     print(ds)
+    return
+
+
+def compute_wrsi_trigger():
     return
 
 
 if __name__ == "__main__":
     compute_spi_trigger()
+    compute_wrsi_trigger()
--- src/process_spi.py
+++ src/process_spi.py
@@ -1,16 +1,10 @@
-import datetime
+import os
+from pathlib import Path
+
 import pandas as pd
 import rioxarray as rxr
-
-from ochanticipy import (
-    create_country_config,
-    CodAB,
-    GeoBoundingBox,
-)
-
 from dotenv import load_dotenv
-from pathlib import Path
-import os
+from ochanticipy import CodAB, create_country_config
 
 load_dotenv()
 
@@ -25,16 +19,23 @@ def process_spi():
     codab = CodAB(country_config=country_config)
     gdf_adm0 = codab.load()
 
-    filenames = [file for file in os.listdir(NEW_CMORPH_DIR) if file.endswith(".tif")]
+    filenames = [
+        file for file in os.listdir(NEW_CMORPH_DIR) if file.endswith(".tif")
+    ]
     for filename in filenames:
-        datestr = filename.removeprefix("GLOBAL-NOAA_CPC_CMORPH-spi-1mo_").removesuffix(".tif")
+        datestr = filename.removeprefix(
+            "GLOBAL-NOAA_CPC_CMORPH-spi-1mo_"
+        ).removesuffix(".tif")
         date = pd.to_datetime(datestr, format="%Y-%m-%d")
         da = rxr.open_rasterio(NEW_CMORPH_DIR / filename)
         da = da.rio.clip(gdf_adm0["geometry"], all_touched=True)
         da = da.expand_dims(dim={"date": [date]})
-        print(da)
-        savename = f"bfa{filename.removeprefix('GLOBAL').removesuffix('.tif')}.nc"
-        da.to_netcdf(SPI_PROC_DIR / savename)
+        da = da.sel(band=1).drop("band")
+        ds = da.to_dataset(name="spi_1")
+        savename = (
+            f"bfa{filename.removeprefix('GLOBAL').removesuffix('.tif')}.nc"
+        )
+        ds.to_netcdf(SPI_PROC_DIR / savename)
 
 
 if __name__ == "__main__":
